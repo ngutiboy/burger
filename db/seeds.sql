588e9d0aec63d45692491ff08d9c3bb8f8d0d192
ngutiboy <35250617+ngutiboy@users.noreply.github.com>
2019-03-10 03:34:46 -0500

    original

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Butter burger',  true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus burger',  true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('50/50 burger', false);
