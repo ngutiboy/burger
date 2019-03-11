5befeead19195f689bc124a98c70c786b6a6e1c2
ngutiboy <35250617+ngutiboy@users.noreply.github.com>
2019-03-10 23:43:50 -0500

    changes

---
+++ README.md
@@ -0,0 +1,6 @@
+Burger App
+
+The essence of this assignment is to create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM. Node and MySQL are used to query, and  Handlebars are used to generate the HTML.
+
+How The App Should Work
+This is a restaurant app that lets users input the names of burgers they'd like to eat. Whenever a user submits a burger's name, this app will display the burger on the top section  of the page -- waiting to be devoured. Each burger in the waiting area also has a Devour it! button. When the user clicks it, the burger will move to the bottom section of the page. This App stores every burger in a database, whether devoured or not.
--- server.js
+++ server.js
@@ -1,49 +1,3 @@
-// // Pull in dependencies
-// var express = require("express");
-// var bodyParser = require('body-parser');
-// var methodOveride = require('method-overide')
-
-
-// var PORT = process.env.PORT || 8080;
-
-// var app = express();
-
-// // // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
-
-// // Parse application body
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
-
-// // // Set Handlebars.
-// var exphbs = require("express-handlebars");
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/burger_controller.js");
-
-// app.use(routes);
-
-// // // Start our server so that it can begin listening to client requests.
-// app.listen(PORT, function() {
-//   // Log (server-side) when our server has started
-//   console.log("Server listening on: http://localhost:" + PORT);
-// });
-
-
-
-
-
-
-
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
@@ -76,7 +30,17 @@ app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
-// app.listen(port);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
